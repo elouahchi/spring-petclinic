92cb9d1be0ff42e0e041df3c7df68db6e637ea6f
El Ouahchi Mohamed Aziz <m.elouahchi@studenti.unisa.it>
2025-12-10 12:40:08 +0100

    Add CheckOverdueVisits helper class with unit tests

--- src/test/java/org/springframework/samples/petclinic/MyCustomTest.java
+++ src/test/java/org/springframework/samples/petclinic/MyCustomTest.java
@@ -3,7 +3,7 @@ package org.springframework.samples.petclinic;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MyCustomTests {
+public class MyCustomTest {
 
 	@Test
 	public void sampleTest() {
